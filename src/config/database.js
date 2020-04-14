3a971f4d4f0d211236156d88ca0a70861ae5e615
Diego Almeida <dyhalmeida@gmail.com>
2020-04-14 12:14:24 -0300

    Configuração de conexão com base de dados Postgres

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
+};
